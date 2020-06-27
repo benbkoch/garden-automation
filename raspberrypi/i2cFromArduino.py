ea4e0c30c4a95fdba2b9f5b5f36afff4ed546cca
Ben Koch <benbkoch@gmail.com>
2020-06-26 17:34:00 -0700

    Fix bytes

--- raspberrypi/i2cFromArduino.py
+++ raspberrypi/i2cFromArduino.py
@@ -9,7 +9,8 @@ while 1:
     try:
         with SMBusWrapper(1) as bus:
             data = bus.read_i2c_block_data(address, 99, 2)
-            print('Offset {}, data {}'.format(data[0], data[1]))
+            sensorValue = data[0] * 256 + data[1]
+            print(sensorValue)
     except:
         print('Oops! Error')
     sleep(5)
