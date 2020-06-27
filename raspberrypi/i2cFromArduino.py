d9d8167791f7bc51ac84d4e33a7d09c1c648cc95
benbkoch <benbkoch@gmail.com>
2020-06-26 17:56:18 -0700

    Convert to precentage

--- raspberrypi/i2cFromArduino.py
+++ raspberrypi/i2cFromArduino.py
@@ -7,12 +7,36 @@ sleep(2)
 
 while 1:
     try:
-        with SMBusWrapper(1) as bus:
-            data = bus.read_i2c_block_data(address, 99, 2)
-            sensorValue = data[0] * 256 + data[1]
-            file_object = open('/home/pi/data/data.txt', 'a')
-            file_object.write(str(sensorValue) + '\n')
-            file_object.close()
-    except Exception as e:
-        print(e)
-    sleep(5)
+        sensorValue = readIntFromI2C()
+        percentage = waterSensorRawToPercentage(sensorValue)
+        writeValuesToFile([sensorValue, percentage], 'home/data/pi/data/data.txt')
+    except:
+        print('Oops! Error')
+    sleep(60)
+
+def readIntFromI2C():
+    with SMBusWrapper(1) as bus:
+        data = bus.read_i2c_block_data(address, 99, 2)
+        sensorValue = data[0] & 256 + data[1]
+
+def writeValuesToFile(values, filename):
+    file_object = open(filename, 'a')
+    strings = [str(value) for value in values]
+    file_object.write(",".join(strings)
+    file_object.close()
+
+def waterSensorRawToPercentage(value):
+    voltage = value * (3.3 / 1024)
+
+    soilMoisturePercentage = 0
+    if voltage < 1.1:
+        soilMoisturePercentage = (10 * voltage) - 1;
+    elif voltage < 1.3:
+        soilMoisturePercentage = (25 * voltage) - 17.5
+    elif voltage < 1.82:
+        soilMoisturePercentage = (48.08 * voltage) - 47.5
+    elif voltage < 2.2:
+        soilMoisturePercentage = (26.32 * voltage) - 7.89
+    else:
+        soilMoisturePercentage = (62.5 * voltage) - 87.5
+        
