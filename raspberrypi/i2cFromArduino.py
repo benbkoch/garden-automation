37529ae9c4b3be298f21a1ae11ddee4503f93136
Ben Koch <benbkoch@gmail.com>
2020-06-26 17:36:58 -0700

    Write to file

--- raspberrypi/i2cFromArduino.py
+++ raspberrypi/i2cFromArduino.py
@@ -10,7 +10,9 @@ while 1:
         with SMBusWrapper(1) as bus:
             data = bus.read_i2c_block_data(address, 99, 2)
             sensorValue = data[0] * 256 + data[1]
-            print(sensorValue)
+            file_object = open('~/data/data.txt', 'a')
+            file_object.write(sensorValue)
+            file_object.close()
     except:
         print('Oops! Error')
-    sleep(5)
+    sleep(60)
