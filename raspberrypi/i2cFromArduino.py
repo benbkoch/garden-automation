f05a7afeafcd07cd6bc32b77b280ca28c5fdb083
Ben Koch <benbkoch@gmail.com>
2020-07-03 11:45:05 -0700

    I don't remember what I changed

--- raspberrypi/i2cFromArduino.py
+++ raspberrypi/i2cFromArduino.py
@@ -5,32 +5,25 @@ address = 0x8
 
 sleep(2)
 
-while 1:
-    try:
-        sensorValue = readIntFromI2C()
-        percentage = waterSensorRawToPercentage(sensorValue)
-        writeValuesToFile([sensorValue, percentage], 'home/data/pi/data/data.txt')
-    except:
-        print('Oops! Error')
-    sleep(60)
-
 def readIntFromI2C():
     with SMBusWrapper(1) as bus:
         data = bus.read_i2c_block_data(address, 99, 2)
-        sensorValue = data[0] & 256 + data[1]
+        sensorValue = data[0] * 256 + data[1]
+        return sensorValue
 
 def writeValuesToFile(values, filename):
     file_object = open(filename, 'a')
     strings = [str(value) for value in values]
-    file_object.write(",".join(strings)
+    file_object.write(",".join(strings) + '\n')
     file_object.close()
 
 def waterSensorRawToPercentage(value):
     voltage = value * (3.3 / 1024)
+    print(voltage)
 
     soilMoisturePercentage = 0
     if voltage < 1.1:
-        soilMoisturePercentage = (10 * voltage) - 1;
+        soilMoisturePercentage = (10 * voltage) - 1
     elif voltage < 1.3:
         soilMoisturePercentage = (25 * voltage) - 17.5
     elif voltage < 1.82:
@@ -39,4 +32,16 @@ def waterSensorRawToPercentage(value):
         soilMoisturePercentage = (26.32 * voltage) - 7.89
     else:
         soilMoisturePercentage = (62.5 * voltage) - 87.5
-        
+    return soilMoisturePercentage
+
+
+while 1:
+    try:
+        sensorValue = readIntFromI2C()
+        percentage = waterSensorRawToPercentage(sensorValue)
+        writeValuesToFile([sensorValue, percentage], '/home/pi/data/data.txt')
+    except Exception as e:
+        print(e)
+    sleep(60)
+
+
